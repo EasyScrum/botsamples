9a0daf149b32d817d2530a56052ca8d15277779d
Mikhail_Krutalevich <mikhail_krutalevich@epam.com>
2020-06-22 21:17:36 +0300

    Samples added

---
+++ csharp_dotnetcore/54.teams-task-module/Bots/TeamsTaskModuleBot.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using Newtonsoft.Json;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class TeamsTaskModuleBot : TeamsActivityHandler
+    {
+        protected override async Task OnTeamsMembersAddedAsync(IList<TeamsChannelAccount> teamsMembersAdded, TeamInfo teamInfo, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var reply = MessageFactory.Attachment(this.GetTaskModuleHeroCard());
+            await turnContext.SendActivityAsync(reply);
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var reply = MessageFactory.Attachment(this.GetTaskModuleHeroCard());
+            await turnContext.SendActivityAsync(reply);
+        }
+
+        protected override async Task<TaskModuleResponse> OnTeamsTaskModuleFetchAsync(ITurnContext<IInvokeActivity> turnContext, TaskModuleRequest taskModuleRequest, CancellationToken cancellationToken)
+        {
+            var reply = MessageFactory.Text("OnTeamsTaskModuleFetchAsync TaskModuleRequest: " + JsonConvert.SerializeObject(taskModuleRequest));
+            await turnContext.SendActivityAsync(reply);
+
+            return new TaskModuleResponse
+            {
+                Task = new TaskModuleContinueResponse
+                {
+                    Value = new TaskModuleTaskInfo()
+                    {
+                        Card = CreateAdaptiveCardAttachment(),
+                        Height = 200,
+                        Width = 400,
+                        Title = "Adaptive Card: Inputs",
+                    },
+                },
+            };
+        }
+
+        protected override async Task<TaskModuleResponse> OnTeamsTaskModuleSubmitAsync(ITurnContext<IInvokeActivity> turnContext, TaskModuleRequest taskModuleRequest, CancellationToken cancellationToken)
+        {
+            var reply = MessageFactory.Text("OnTeamsTaskModuleSubmitAsync Value: " + JsonConvert.SerializeObject(taskModuleRequest));
+            await turnContext.SendActivityAsync(reply);
+
+            return new TaskModuleResponse
+            {
+                Task = new TaskModuleMessageResponse()
+                {
+                    Value = "Thanks!",
+                },
+            };
+        }
+
+        private Attachment GetTaskModuleHeroCard()
+        {
+            return new HeroCard()
+            {
+                Title = "Task Module Invocation from Hero Card",
+                Subtitle = "This is a hero card with a Task Module Action button.  Click the button to show an Adaptive Card within a Task Module.",
+                Buttons = new List<CardAction>()
+                    {
+                        new TaskModuleAction("Adaptive Card", new { data = "adaptivecard" }),
+                    },
+            }.ToAttachment();
+        }
+
+        private static Attachment CreateAdaptiveCardAttachment()
+        {
+            // combine path for cross platform support
+            string[] paths = { ".", "Resources", "adaptiveCard.json" };
+            var adaptiveCardJson = File.ReadAllText(Path.Combine(paths));
+
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+    }
+}
